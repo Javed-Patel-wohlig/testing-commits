82be06d67ee049064b4f0c41c2316a57573d3121
Javed-Patel-wohlig <javed.patel@wohlig.com>
2024-01-10 18:03:01 +0530

    with python

---
+++ update_file.py
@@ -0,0 +1,19 @@
+import os
+
+file_path = "count.txt"
+
+# Read current count from the file
+with open(file_path, "r") as file:
+    count = int(file.read().split(":")[1].strip())
+
+# Update count
+count += 50
+
+# Update file content
+with open(file_path, "w") as file:
+    file.write(f"hello i am devops\nmy commit count is: {count}\nthank you")
+
+# Commit and push changes
+os.system("git add .")
+os.system("git commit -m 'Update commit count'")
+os.system(f"git push https://Javed-Patel-wohlig:${{ secrets.TOKEN100 }}@github.com/Javed-Patel-wohlig/test-gitops.git HEAD:main")
